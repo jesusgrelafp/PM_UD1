55ccc1b17c2fb4354a55f4c563e967fafe156b19
Jesús <jesusgrelafp@gmail.com>
2025-09-11 12:57:43 +0200

    EntradaSalida

---
+++ src/elementos_basicos/EntradaSalida.kt
@@ -0,0 +1,19 @@
+package elementos_basicos
+
+fun main() {
+/*
+    // Mostrar datos al usuario
+    val levelUpMessage = "¡Subiste de nivel!"
+    val newAbility = "Habilidad aprendida"
+
+    println(levelUpMessage)
+    println(newAbility)
+
+ */
+
+    // Leer entradas del usuario
+    print("ponle un nombre a tu personaje: ")
+    val pjName = readLine()
+
+    println("Bienvenido $pjName!")
+}
