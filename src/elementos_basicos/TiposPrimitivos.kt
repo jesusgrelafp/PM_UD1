a29d718b23eacc4c129df13410c3a21def1977e0
Jesús <jesusgrelafp@gmail.com>
2025-09-11 12:22:30 +0200

    Tipos Primitivos

---
+++ src/elementos_basicos/TiposPrimitivos.kt
@@ -0,0 +1,54 @@
+package elementos_basicos
+
+import kotlin.reflect.typeOf
+
+fun main() {
+
+/*
+    // 1. Tipo enteros
+    val level: Byte = 50
+    val health: Short = 150
+    val missionPoints: Int = 40000
+    val experience: Long = 45121365412
+    val monstersExecuted = 9990001522L // La 'L' especifica el tipo Long
+
+    print(monstersExecuted::class)
+*/
+
+/*
+    //Literales binarios y hexadecimales
+
+    val bitmapLocation = 0b00100001 // Literal binario 0b ó 0B
+    val chestColor = 0xCCC  // Literal hexadecimal 0x ó 0X
+                            // C es 12 en hex
+                            //  12*16^2+12*16^1+12*16^0
+    println (bitmapLocation)
+    println (chestColor)
+
+    val attackSpeed: Float = 0.5f
+    val dodgeChance: Double = 0.2
+*/
+
+/*
+    val exp1 = 3.211e2
+    val exp2 = .0001e10
+    val exp3 = 48e5
+    val exp4 = 10e-4
+    val exp5 = 1.01e2
+    val exp6 = 101e-2
+
+    println(exp1)
+    println(exp2)
+    println(exp3)
+    println(exp4)
+    println(exp5)
+    println(exp6)
+
+*/
+
+    val response : Char = 'Y'
+    println(response)
+
+    print("Uno\nDos\nTres")
+
+}
