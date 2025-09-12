151dcb32c14442e6eff864a445a5e9324bc164ef
Jesús <jesusgrelafp@gmail.com>
2025-09-12 09:51:24 +0200

    Arrays

---
+++ src/elementos_basicos/Arrays.kt
@@ -0,0 +1,48 @@
+package elementos_basicos
+
+fun main() {
+
+    // Crear Arrays
+    val income = arrayOf<Double>(
+        0.5, 2.5, 4.0, 5.0,
+        4.5, 6.0, 7.6, 8.0,
+        5.0, 6.4, 4.0, 9.1
+    )
+    val income2 = arrayOf(0.5, 2.5, 4.0, 5.0)
+
+    // Constructor Array
+    val negativeNumbers = Array(10) { -(it+1) }
+    val negativeNumbers2 = Array(10) { i:Int -> -(i+1) }
+    println(negativeNumbers.joinToString ())
+    println(negativeNumbers.contentToString())
+    println(negativeNumbers2.contentToString())
+
+    // Acceso a los elementos
+    val planets = arrayOfNulls<String>(8)
+    planets[0] = "Mercurio"
+    planets[1] = "Venus"
+    planets[2] = "Tierra"
+    planets[3] = "Júpiter"
+    planets[4] = "Saturno"
+    planets[5] = "Urano"
+    planets[6] = "Neptuno"
+    planets[7] = "Plutón"
+    println(planets.contentToString())
+    println("En la posición 5 está: ${planets.get(5)}")
+    planets.set(7,"Desaparecido")
+    println(planets.get(7))
+
+    for (i in planets.indices) {
+        println("For In --> ${planets[i]} esté en la posición ${i+1}")
+    }
+
+    for (i in 0 until planets.size) {
+       println("for In Until --> ${planets[i]} esté en la posición ${i+1}")
+    }
+
+    planets.forEachIndexed { i,v ->
+        println("forEachIndexed -->  $v está en la posición ${i+1}")
+    }
+}
+
+
