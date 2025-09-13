a92f4a3568aa8982acfddd30a36f5677d789cb74
Jesús <jesusgrelafp@gmail.com>
2025-09-13 20:54:42 +0200

    CalculadoraNullable

---
+++ src/practicas/CalculadoraNullable.kt
@@ -0,0 +1,24 @@
+class CalculadoraNullable {
+
+    // Suma
+    fun sumar(a: Int?, b: Int?): Int {
+        return (a ?: 0) + (b ?: 0)
+    }
+
+    // Resta
+    fun restar(a: Int?, b: Int?): Int {
+        return (a ?: 0) - (b ?: 0)
+    }
+
+    // Multiplicación
+    fun multiplicar(a: Int?, b: Int?): Int {
+        return (a ?: 0) * (b ?: 0)
+    }
+
+    // División
+    fun dividir(a: Int?, b: Int?): Int {
+        val divisor = b ?: 0
+        if (divisor == 0) throw IllegalArgumentException("No se puede dividir por cero o null")
+        return (a ?: 0) / divisor
+    }
+}
---
+++ src/practicas/Practica5.kt
@@ -0,0 +1,22 @@
+package practicas
+import CalculadoraNullable
+
+fun main() {
+
+    print("Introduzca el primer entero: ")
+    val a : Int? = readLine()?.toIntOrNull()
+
+    print("Introduzca el segundo entero: ")
+    val b : Int? = readLine()?.toIntOrNull()
+
+    val calculadora = CalculadoraNullable()
+
+    println("suma = ${calculadora.sumar(a, b)}")
+    println("resta = ${calculadora.restar(a, b)}")
+    println("multiplica = ${calculadora.multiplicar(a, b)}")
+    try {
+        println("divide = ${calculadora.dividir(a, b)}")
+    } catch (e: IllegalArgumentException) {
+        println("Error: ${e.message}")
+    }
+}
