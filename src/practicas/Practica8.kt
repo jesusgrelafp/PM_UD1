bd8b725ba16447cce8a487e34614c1d02e3821f4
Jesús <jesusgrelafp@gmail.com>
2025-09-14 21:30:38 +0200

    Práctica 8

---
+++ src/practicas/Practica8.kt
@@ -0,0 +1,20 @@
+package practicas
+
+fun main() {
+    print("Introduce un número positivo: ")
+    val n = (readLine()?:"0").toInt()
+
+    for (i in 1..n) {
+        if (i % 3 == 0) {
+            continue // saltar múltiplos de 3
+        }
+
+        if (i == 20) {
+            println("Se alcanzó el 20, fin del bucle.")
+            break // salir del bucle
+        }
+
+        println(i)
+    }
+}
+
