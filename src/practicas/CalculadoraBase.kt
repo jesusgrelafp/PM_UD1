88ef025a6a9bb0eace03415b22f6f56c2d2137a9
Jesús <jesusgrelafp@gmail.com>
2025-09-22 13:20:15 +0200

    Corregir función dividir

--- src/practicas/CalculadoraBase.kt
+++ src/practicas/CalculadoraBase.kt
@@ -6,7 +6,8 @@ open class CalculadoraBase (val a: Double, val b: Double) {
     fun sumar() = a+b
     fun restar() = a-b
     fun multiplicar() = a*b
-    fun dividir() = a/b
+    fun dividir() = if (b == 0.0) throw IllegalArgumentException("No se puede dividir por cero o null")
+                    else a / b
 }
 
 class CalculadoraCientifica(a: Double, b: Double) : CalculadoraBase(a,b) {
