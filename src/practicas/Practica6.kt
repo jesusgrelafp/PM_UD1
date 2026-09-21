69b29fac649bc66bf50eccfb9c3afeb60acb92b0
gcj <jesusgrelafp@gmail.com>
2026-09-21 17:38:56 +0200

    Otra versión práctica7

--- src/practicas/Practica6.kt
+++ src/practicas/Practica6.kt
@@ -17,3 +17,24 @@ fun main() {
         imc >= 30 -> println("Obesidad")
     }
 }
+
+/*  OTRA VERSIÓN
+
+fun main() {
+    print("Introduzca el peso en Kg: ")
+    val pesoKg : Float = (readlnOrNull()?: "0").toFloat()
+
+    print("Introduzca la altura en metros: ")
+    val alturaM : Float = (readlnOrNull()?: "0").toFloat()
+
+    val imc = pesoKg / (alturaM * alturaM)
+    println("El IMC es: $imc")
+
+    when (imc) {
+        in 0.0f..<18.5f -> println("Bajo peso")
+        in 18.5f..<25.0f -> println("Normal")
+        in 25.0f..<30.0f -> println("Sobrepeso")
+        else -> println("Obesidad")
+    }
+}
+*/
